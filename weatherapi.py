794cad58efdada542dce7975fe67f0f46a9701a1
ankita93anki <ankitasingh7639369@gmail.com>
2026-06-23 18:19:10 +0530

    weather API

---
+++ weatherapi.py
@@ -0,0 +1,46 @@
+
+#Weather App using OpenWeatherMap API
+import requests
+
+#Step 1: API SETUP
+API_KEY="93f19e6a7f7fe65e59dee403828815f2"
+BASE_URL=f"https://api.openweathermap.org/data/2.5/weather"
+
+#Step 2: Get Weather Data
+def get_weather(city):
+    try:
+        url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                "City": data['name'],
+                "Temperature": f"{data["main"]["temp"]}C",
+                "Weather": data["weather"][0]["description"].title(),
+                "Humidity": f"{data['main']['humidity']}%",
+                "Wind Speed": f"{data["wind"]["speed"]}m/s"
+            }
+            return weather
+        elif response.status_code == 404:
+            print("City not found.")
+        else:
+            print("An error occurred. Status code:", response.status_code)
+    except Exception as e:
+        print("An error occurred:", e)
+    return None
+
+#Step 3: Display Weather Information
+def display_weather(weather):
+    print("\n---- Weather Information -----")
+    for key, value in weather.items():
+        print(f"{key}:{value}")
+
+#Step 4: Main Prog Loop
+while True:
+    print("\n---- Weather App-----")
+    city = input("Enter a city name (or 'q' to quit) ").strip()
+    if city.lower() == 'q':
+        break 
+    weather = get_weather(city)
+    if weather:
+        display_weather(weather)
